80b6799de01b4763492c47769dd552e2c6fd4797
Jose Sergio Filho <jose.sergi@outlook.com>
2024-11-16 16:47:54 -0300

    Configurando para deploy

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login_view, name='login'),
     path('sair/', views.sair, name='sair'),
